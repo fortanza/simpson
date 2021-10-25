8688491199255e23e0b438c79ad786d1a9e80df3
fortanza <undertaker040880@gmail.com>
2021-10-25 20:37:22 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  
+  const [simpsonQuotes, setsimpsonQuotes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes?count=30')
+      .then(({ data }) => {
+        setsimpsonQuotes(data.results);
+      })
+      .catch(() => {
+        console.error('Plz fix your call, or set up your internet');
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Task : Simpsons Quote</h1>
+     
+     <button type="button">Get new Quote</button>
     </div>
   );
 }
