360132e43a5b57988e9c217db389d24406b5a778
fortanza <undertaker040880@gmail.com>
2021-10-25 21:09:16 +0200

    Modif App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
+import QuoteCard from './QuoteCard';
+
 function App() {
-  
+
   const [simpsonQuotes, setsimpsonQuotes] = useState([]);
 
   useEffect(() => {
     axios
       .get('https://simpsons-quotes-api.herokuapp.com/quotes?count=30')
       .then(({ data }) => {
-        setsimpsonQuotes(data.results);
+        setsimpsonQuotes(data);
       })
       .catch(() => {
         console.error('Plz fix your call, or set up your internet');
@@ -20,7 +22,10 @@ function App() {
   return (
     <div className="App">
      <h1>Task : Simpsons Quote</h1>
-     
+   
+     {simpsonQuotes.map((quote, index) => (
+        <QuoteCard key={index} {...quote} />
+      ))}
      <button type="button">Get new Quote</button>
     </div>
   );
